5cf1e210dc763633b3cfcc7e5cf1470e78e1f029
Caio Schuu <caioschuu@gmail.com>
2024-04-10 19:42:26 -0300

    Project created

---
+++ src/main/java/com/caioschuu/api_restfulmongo/ApiRestfulmongoApplication.java
@@ -0,0 +1,13 @@
+package com.caioschuu.api_restfulmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestfulmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestfulmongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/caioschuu/api_restfulmongo/ApiRestfulmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caioschuu.api_restfulmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestfulmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
